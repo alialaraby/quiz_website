d0204846729e0a9139dc90e3ac324564e23d2969
ali saadawy alaraby <merge.conflictstyle>
2019-05-03 21:54:49 +0200

    added logged user info to the profile

--- profile.js
+++ profile.js
@@ -18,6 +18,32 @@ $(document).ready(function () {
             createQuizItem(takenQuizesArray[i].quizName, 50);
         }
     }
+
+
+    //create users array 
+    usersLogedinArray = [];
+    userMail = '';
+    //check if the object in the local storage is already created 
+    //or this is the first time creating it(local storage is empty)
+    if (localStorage.getItem('user_signed') === null){
+        //case local storage is empty, leave the array empty
+        //this step is for avoiding 'array is null' exception
+    }else{
+        //case local storage already has the object, then 
+        //get all the values in it to add to it to get the previous values
+        usersLogedinArray = JSON.parse(localStorage.getItem('user_signed'));
+        userMail = usersLogedinArray.email;
+    }
+
+
+    //create users array 
+    usersArray = checkItemInLocalStorage('user');
+    for (var i = 0; i < usersArray.length; i++) {
+        if (usersArray[i].email == userMail) {
+            $('#name span').text(usersArray[i].fname + " " + usersArray[i].lname);
+            $('#email span').text(userMail);
+        }        
+    }
     
 })
 
@@ -26,4 +52,17 @@ function createQuizItem(quizName, quizGrade) {
     newQuizCard.innerHTML = "<li class='list-group-item'><i class='fas fa-file-alt'></i> "+ quizName +" : " + quizGrade + "</li>"
     $(newQuizCard).css("list-style", 'none');
     $('#grade_list_group').append(newQuizCard);
+}
+function checkItemInLocalStorage(key) {
+    var arrayToCheck = [];
+
+    if (localStorage.getItem(key) === null){
+        //case local storage is empty, leave the array empty
+        //this step is for avoiding 'array is null' exception
+    }else{
+        //case local storage already has the object, then 
+        //get all the values in it to add to it to get the previous values
+        arrayToCheck = JSON.parse(localStorage.getItem(key))
+    }    
+    return arrayToCheck;
 }
